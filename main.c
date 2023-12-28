aff60e6a62b84abbdccb323c9cefe4fb94b09b53
clovis <clovis-p@proton.me>
2023-12-27 19:04:16 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+//
+// Created by clovis on 12/27/23.
+//
+
+#include <stdio.h>
+
+#include "main.h"
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by clovis on 12/27/23.
+//
+
+#ifndef MANDELBROT_MAPPER_MAIN_H
+#define MANDELBROT_MAPPER_MAIN_H
+
+#endif //MANDELBROT_MAPPER_MAIN_H
