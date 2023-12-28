46249c6b3fbc30d0c1402f6a17146c505db8c822
clovis <clovis-p@proton.me>
2023-12-27 19:33:14 -0500

    Empty window

--- main.c
+++ main.c
@@ -4,10 +4,32 @@
 
 #include <stdio.h>
 
+#include <SDL2/SDL.h>
+
 #include "main.h"
 
 int main()
 {
-    printf("Hello, World!\n");
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* win = SDL_CreateWindow("Mandelbrot Mapper", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                       640,480,SDL_WINDOW_SHOWN);
+
+    if (win == NULL)
+    {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+
     return 0;
 }
